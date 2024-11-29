aca3b87950bd18ba4bcdb174daf315deb10c2ce8
Radoslaw <radoslaw.miczulski@gmail.com>
2024-11-29 15:23:50 +0100

    Header-start

--- rado.dyntro.client/src/app/app.module.ts
+++ rado.dyntro.client/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HeaderComponent } from './header/header.component'
+import { MainComponent } from './main/main.component'
+import { FooterComponent } from './footer/footer.component'
+import { NavigationComponent } from './header/navigation/navigation.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavigationComponent,
+
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ rado.dyntro.client/src/app/header/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: false,
+  
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+
+}
