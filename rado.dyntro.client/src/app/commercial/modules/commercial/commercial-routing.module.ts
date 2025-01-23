37182cb2ef62294916fb89532cab177b09072cd7
Radoslaw <radoslaw.miczulski@gmail.com>
2025-01-23 23:04:04 +0100

    Routing changes part 2

--- rado.dyntro.client/src/app/commercial/modules/commercial/commercial-routing.module.ts
+++ rado.dyntro.client/src/app/commercial/modules/commercial/commercial-routing.module.ts
@@ -1,12 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommercialComponent } from '../../commercial.component';
-import { FooterComponent } from '../../footer/footer.component';
-import { HeaderComponent } from '../../header/header.component';
-import { NavigationComponent } from '../../header/navigation/navigation.component';
 import { MainComponent } from '../../main/main.component';
+import { SectionAboutUsComponent } from '../../main/section-about-us/section-about-us.component';
+import { SectionColdMailingOfferComponent } from '../../main/section-cold-mailing-offer/section-cold-mailing-offer.component';
+import { SectionContactComponent } from '../../main/section-contact/section-contact.component';
+import { SectionLinkedinAutoOfferComponent } from '../../main/section-linkedin-auto-offer/section-linkedin-auto-offer.component';
+import { SectionMajorComponent } from '../../main/section-major/section-major.component';
+import { SectionClientsComponent } from '../../main/section-clients/section-clients.component';
+import { SectionLoginComponent } from '../../main/section-login/section-login.component';
+import { ClientPersonnelServiceComponent } from '../../main/section-specific-client/client-personnel-service/client-personnel-service.component';
+import { ClientArgentumEventComponent } from '../../main/section-specific-client/client-argentum-event/client-argentum-event.component';
+import { ClientPragmaGoComponent } from '../../main/section-specific-client/client-pragma-go/client-pragma-go.component';
+import { ClientVendoriseComponent } from '../../main/section-specific-client/client-vendorise/client-vendorise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: CommercialComponent,
+    children: [
+      { path: '', redirectTo: 'main', pathMatch: 'full' },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+      { path: 'main', component: MainComponent },
+
+    ]
+  }
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
