9210b945f7f212162cb8c3307fa93fee4d0d2b65
Radoslaw <radoslaw.miczulski@gmail.com>
2025-01-30 22:15:31 +0100

    t

---
+++ Rado.Dyntro.Server/Order/Controller/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rado.Dyntro.Server.Order.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class OrderController
+    {
+    }
+}
---
+++ Rado.Dyntro.Server/Order/ViewModel/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Rado.Dyntro.Server.Order.ViewModel
+{
+    public class OrderViewModel
+    {
+        public int ID { get; set; }
+        
+        public string? Status { get; set; }
+
+        public string? Topic { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? Priority { get; set; }
+
+        public DateOnly Date { get; set; }
+     
+
+    }
+}
