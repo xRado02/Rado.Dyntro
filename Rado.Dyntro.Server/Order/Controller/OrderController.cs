4db06b241ea07fc4301eeece6a638065f979dae3
Radoslaw <radoslaw.miczulski@gmail.com>
2025-01-31 22:49:24 +0100

    Trying to connect back with frontend. Before edit module dashboard

--- Rado.Dyntro.Server/Order/Controller/OrderController.cs
+++ Rado.Dyntro.Server/Order/Controller/OrderController.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rado.Dyntro.Server.Order.ViewModel;
 
 namespace Rado.Dyntro.Server.Order.Controller
 {
     [ApiController]
     [Route("[controller]")]
 
-    public class OrderController
+    public class OrderController : ControllerBase
     {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;   
+        }
+
+        [HttpGet(Name = "GetOrder")]
+
+        public IEnumerable<OrderViewModel> Get()
+        {
+            return Enumerable.Range(1, 10).Select(index => new OrderViewModel
+            {
+                ID = index,
+                Status = "Active",
+                Topic = "None",
+                FirstName = "Jhon",
+                LastName = "Jhonson",
+                Category = "Test",
+                Priority = "Top",
+                Date = DateOnly.FromDateTime(DateTime.Now)
+,
+            })
+            .ToArray();
+            
+        }
+
+
+
     }
 }
--- rado.dyntro.client/src/app/dashboard/components/main/orders/orders.component.ts
+++ rado.dyntro.client/src/app/dashboard/components/main/orders/orders.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+
+interface Order {
+
+  id: number;
+  status: string;
+  topic: string;
+  firstName: string;
+  lastName: string;
+  category: string;
+  priority: string;
+  date: Date;
+}
+
 
 @Component({
   selector: 'app-orders',
@@ -7,6 +22,16 @@ import { Component } from '@angular/core';
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+
+  public order: Order[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    
+  }
 
 }
