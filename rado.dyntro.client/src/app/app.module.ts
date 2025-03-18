30098565bf8bf0c1cfd7ca5f27ad56d5d51399cc
Radoslaw <radoslaw.miczulski@gmail.com>
2025-03-18 16:17:13 +0100

    add PathLocationStrategy

--- rado.dyntro.client/src/app/app.module.ts
+++ rado.dyntro.client/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common'; // Dodaj ten import
 
 @NgModule({
   declarations: [
     AppComponent,
-  
-   
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     AppRoutingModule,
- 
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy }, // Dodaj ten dostawcę
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
